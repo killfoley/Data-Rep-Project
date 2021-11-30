151b0e303cd1c2ba6c23620ee7797cf6c09dce5f
Killian Foley <killfoley87@gmail.com>
2021-11-30 22:56:59 +0000

    created an init.sql file and started on DAO file

---
+++ code/DAO.py
@@ -0,0 +1,78 @@
+import mysql.connector
+import dbconfig as cfg
+
+# Create a Database Access Object Class
+class HardwareDAO:
+    db=""
+    # Initiate the connection
+    def __init__(self): 
+        self.db = mysql.connector.connect(
+        host=       cfg.mysql['host'],
+        user=       cfg.mysql['user'],
+        password=   cfg.mysql['password'],
+        database=   cfg.mysql['database']
+        )
+    
+    # Create function for creating entries        
+    def create(self, values):
+        cursor = self.db.cursor()
+        sql="insert into BLANK (title,author, price) values (%s,%s,%s)"
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        return cursor.lastrowid
+
+    # Function for getting all entries from database
+    def getAll(self):
+        cursor = self.db.cursor()
+        sql="select * from BLANK"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        print(results)
+        for result in results:
+            print(result)
+            returnArray.append(self.convertToDictionary(result))
+        return returnArray
+
+    # Function to search by Product ID
+    def findByID(self, id):
+        cursor = self.db.cursor()
+        sql="select * from BLANK where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        return self.convertToDictionary(result)
+
+    # Function to update existing entries
+    def update(self, values):
+        cursor = self.db.cursor()
+        sql="update BLANK set title= %s,author=%s, price=%s  where id = %s"
+        cursor.execute(sql, values)
+        self.db.commit()
+    
+    #Function to delete entries for DB
+    def delete(self, id):
+        cursor = self.db.cursor()
+        sql="delete from BLANK where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        print("delete done")
+
+    # Function to convert SQL array to Python dict
+    def convertToDictionary(self, result):
+        colnames=['id','Title','Author', "Price"]
+        item = {}
+        
+        if result:
+            for i, colName in enumerate(colnames):
+                value = result[i]
+                item[colName] = value
+        
+        return item
+        
+HardwareDAO = HardwareDAO()
---
+++ code/init.sql
@@ -0,0 +1,44 @@
+--drop database if exists
+drop database IF EXISTS datarepproject;
+
+--create new database
+create database datarepproject;
+
+--initiate new database to add tables
+use datarepproject;
+
+--create table Product
+create table Product(
+Id int NOT NULL AUTO_INCREMENT,
+Name varchar(100),
+Manufacturer varchar(100),
+Supplier varchar(100),
+Primary Key(Id))
+Engine=InnoDB DEFAULT CHARSET=utf8;
+
+--Create table Stock
+create table Stock(
+Id INT NOT NULL,
+SafetyStock INT NULL,
+Stock INT NULL,
+Location varchar(20),
+ProductId INT NULL,
+Primary Key(Id),
+Foreign Key(ProductId) References Product(Id) ON UPDATE CASCADE ON DELETE SET NULL
+)Engine=InnoDB DEFAULT CHARSET=utf8;
+
+--Create table Price
+create table Price(
+Id INT NOT NULL,
+CostPrice DECIMAL(10,2),
+SellPrice DECIMAL(10,2),
+ProductId INT NULL,
+StockId INT NULL,
+Primary Key(Id),
+Foreign Key(ProductId) References Product(Id) ON UPDATE CASCADE ON DELETE SET NULL,
+Foreign Key(StockId) References Stock(Id) ON UPDATE CASCADE ON DELETE SET NULL
+)Engine=InnoDB DEFAULT CHARSET=utf8;
+
+
+/* Insert Product (Name, Manufacturer, Supplier) VALUES ('Impact Driver', 'Makita', 'DrillMen');*/
+
