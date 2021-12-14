5e81c541031854918272f774f2133c45161d4d15
Killian Foley <killfoley87@gmail.com>
2021-12-14 20:32:36 +0000

    made updates to app.py

--- code/app.py
+++ code/app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, g, jsonify, request, abort, make_response, render_template, session, redirect, url_for
 from DAO import HardwareDAO
-# import json
+import json
 
 
 # initiate the Flask server
@@ -56,45 +56,39 @@ def login():
 
     return render_template('login.html')
 
-@app.route('/profile')
-def profile():
+@app.route('/home')
+def home():
     if not g.user:
         return redirect(url_for('login'))
 
-    return render_template('profile.html')
+    return render_template('home.html')
 
-'''
-
-# ---- read ----
-
-@app.route('/equipment')
+# get all stock entries
+@app.route('/stockitem')
 def getAll():
-    # session['username'] = "I dunno"
     if not 'username' in session:
         abort(401)
 
     results = HardwareDAO.getAll()
     return jsonify(results)
 
-
-@app.route('/equipment/<int:id>')
+# find by Id
+@app.route('/stockitem/<int:id>')
 def findById(id):
     if not 'username' in session:
         abort(401)
 
-    foundEquipment = HardwareDAO.findByID(id)
+    stockResult = HardwareDAO.findByID(id)
 
     # Check if id exists
-    if not foundEquipment:
-        return "That id has not been found in the equipment database."
+    if not stockResult:
+        return "Cannot find that product in the database"
         abort(404)
 
-    return jsonify(foundEquipment)
+    return jsonify(stockResult)
 
-
-# ---- create ----
-
-@app.route('/equipment', methods=['POST'])
+# Create new stock item
+@app.route('/stockitem', methods=['POST'])
 def create():
     if not 'username' in session:
         abort(401)
@@ -103,106 +97,107 @@ def create():
     if not request.json:
         return "Wrong request"
         abort(400)
-    # if not 'id' in request.json:
-    #     return "Wrong request (id)"
-    #     abort(400)
 
-    equip = {
-        "category": request.json['category'],
-        "name": request.json['name'],
-        "supplier": request.json['supplier'],
-        "price_eur": request.json['price_eur']
+    product = {
+        "Name": request.json['name'],
+        "Manufacturer": request.json['manufacturer'],
+        "Supplier": request.json['supplier'],
+        "SafetyStock": request.json['safety_stock'],
+        "CurrentStock": request.json['current_stock'],
+        "CostPrice" : request.json['cost_price'],
+        "SellPrice" : request.json['sell_price']
     }
-    # Make the tuple for DB
-    values = (equip['category'], equip['name'],
-              equip['supplier'], equip['price_eur'])
+    # create values for insert into db
+    values = (product['name'], product['manufacturer'],
+              product['supplier'], product['price_eur'], 
+              product['current_stock'],product['cost_price'],
+              product['sell_price'])
     newId = HardwareDAO.create(values)
-    equip['id'] = newId
-    return jsonify(equip)
+    product['id'] = newId
+    return jsonify(product)
 
 
 # sample test
-# curl -i -H "Content-Type:application/json" -X POST -d '{"category":"Tier 2","name":"Elwirka","supplier":"Elwro","price_eur":30000.00}' http://localhost:5000/equipment
+# curl -i -H "Content-Type:application/json" -X POST -d '{"name":"Tier 2","name":"Elwirka","supplier":"Elwro","price_eur":30000.00}' http://localhost:5000/stockitem
 # for windows use this one
-# curl -i -H "Content-Type:application/json" -X POST -d "{\"category\":\"Tier 2\",\"name\":\"Elwirka\",\"supplier\":\"Elwro\",\"price_eur\":30000.00}" http://localhost:5000/equipment
+# curl -i -H "Content-Type:application/json" -X POST -d "{\"category\":\"Tier 2\",\"name\":\"Elwirka\",\"supplier\":\"Elwro\",\"price_eur\":30000.00}" http://localhost:5000/stockitem
 
-# ---- update ----
-
-@app.route('/equipment/<int:id>', methods=['PUT'])
+# App to update a stock item
+@app.route('/stockitem/<int:id>', methods=['PUT'])
 def update(id):
     if not 'username' in session:
         abort(401)
 
-    foundEquipment = HardwareDAO.findByID(id)
+    returnedProduct = HardwareDAO.findByID(id)
 
-    if not foundEquipment:
+    if not returnedProduct:
         return "That id does not exist in the database"
         abort(404)
     if not request.json:
-        return "Wrong request"
+        return "Request Error"
         abort(400)
 
     reqJson = request.json
 
     # checks for data integrity
-    if ('price_eur' in reqJson) and (type(reqJson['price_eur']) is not float):
+    if ('cost_price' in reqJson) and (type(reqJson['cost_price']) is not float):
         abort(400)
-        return "Wrong request or data type (should be float)"
+        return "Data type is incorrect. Please provide decimal value for price"
+    if ('sell_price' in reqJson) and (type(reqJson['sell_price']) is not float):
+        abort(400)
+        return "Data type is incorrect. Please provide decimal value for price"
 
-    if 'category' in request.json:
-        foundEquipment['category'] = reqJson['category']
     if 'name' in request.json:
-        foundEquipment['name'] = reqJson['name']
+        returnedProduct['name'] = reqJson['name']
+    if 'manufacture' in request.json:
+        returnedProduct['manufacturer'] = reqJson['manufacturer']
     if 'supplier' in request.json:
-        foundEquipment['supplier'] = reqJson['supplier']
-    if 'price_eur' in request.json:
-        foundEquipment['price_eur'] = reqJson['price_eur']
+        returnedProduct['supplier'] = reqJson['supplier']
+    if 'safety_stock' in request.json:
+        returnedProduct['safety_stock'] = reqJson['safety_stock']
+    if 'current_stock' in request.json:
+        returnedProduct['current_stock'] = reqJson['current_stock']
+    
 
     # Make the tuple for DB
-    values = (foundEquipment['category'], foundEquipment['name'],
-              foundEquipment['supplier'], foundEquipment['price_eur'], foundEquipment['id'])
+    values = (returnedProduct['name'], returnedProduct['manufacturer'],
+              returnedProduct['supplier'], returnedProduct['price_eur'], 
+              returnedProduct['current_stock'],returnedProduct['cost_price'],
+              returnedProduct['sell_price'])
     # Do the update on DB
     HardwareDAO.update(values)
 
-    return jsonify(foundEquipment)
+    return jsonify(returnedProduct)
 
 # for Linux
-# curl -i -H "Content-Type:application/json" -X PUT -d '{"name":"Odra"}' http://localhost:5000/equipment/5
+# curl -i -H "Content-Type:application/json" -X PUT -d '{"name":"Odra"}' http://localhost:5000/product/5
 # for Windows use this one
-# curl -i -H "Content-Type:application/json" -X PUT -d "{\"name\":\"Odra\"}" http://localhost:5000/equipment/5
+# curl -i -H "Content-Type:application/json" -X PUT -d "{\"name\":\"Odra\"}" http://localhost:5000/productm/5
 
 
 # ---- delete ----
 
-@app.route('/equipment/<int:id>', methods=['DELETE'])
-def delete_equipment(id):
+@app.route('/stockitem/<int:id>', methods=['DELETE'])
+def deleteStockItem(id):
     if not 'username' in session:
         abort(401)
 
-    foundEquipment = HardwareDAO.findByID(id)
-    if not foundEquipment:
+    returnedProduct = HardwareDAO.findByID(id)
+    if not returnedProduct:
         return "That id does not exist in the database"
         abort(404)
     HardwareDAO.delete(id)
     return jsonify({"done": True})
 
 
-# --------------------------------
-# Getting static pages
-# --------------------------------
 
+# Getting static pages
 @app.route('/about')
 def about():
-    # return "inside about()1" # test ok
-    # if not 'username' in session:
-    #     return render_template('index.html')
-
     return render_template('about.html')
 
 
-# --------------------------------
-# Error handling with Flask routes
-# --------------------------------
+# Error handling 
 
 @app.errorhandler(404)
 def not_found404(error):
@@ -214,10 +209,5 @@ def not_found400(error):
     return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
-# ------------------
-# Check dependencies
-# ------------------
-
 if __name__ == '__main__':
     app.run(debug=False) # set for production
-    '''
